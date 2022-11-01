07ab40849dd609ca2fe3add2b4381ae74eb4fd44
debdevs <debdevs97@gmail.com>
2022-11-01 04:46:48 -0400

    create navbar button and add to navbar
    created navbar button component and imported into navbar with updated styling.

---
+++ skyway-visuals-frontend/components/atoms/NavbarButton/NavbarButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './NavbarButton.module.css';
+const NavbarButton = () => {
+  return <button className={styles.navbar_button}>Home</button>;
+};
+
+export default NavbarButton;
--- skyway-visuals-frontend/components/organisms/Navbar/Navbar.jsx
+++ skyway-visuals-frontend/components/organisms/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import Image from 'next/future/image';
 import styles from './Navbar.module.css';
+import NavbarButton from '../../atoms/NavbarButton/NavbarButton';
 const Navbar = () => {
   return (
     <div className={styles.navbar_parent}>
       <div className={styles.navbar_content}>
-        <Image
-          src={require('../../../public/logo.png')}
-          className={styles.navbar_logo_image}
-          alt="Logo"
-          layout="raw"
-        />
+        <left-items>
+          <Image
+            src={require('../../../public/logo.png')}
+            className={styles.navbar_logo_image}
+            alt="Logo"
+            layout="raw"
+          />
+        </left-items>
+        <right-items>
+          <NavbarButton />
+          <NavbarButton />
+          <NavbarButton />
+          <NavbarButton />
+        </right-items>
       </div>
     </div>
   );
